728af15b85fac36b0a278449a60b10365e839899
SamuelRamirezM <156239082+SRamirez967@users.noreply.github.com>
2026-04-02 10:42:21 -0500

    Correcciones en la logica

--- index.js
+++ index.js
@@ -13,14 +13,8 @@ class Usuario {
     static impuesto = 1.19;
 
     // Estadisticas
-    static ganancias = 0;
-    static totalBasico = 0;
-    static totalPro = 0;
-    static totalPremium = 0;
-    static totalAnfitrion = 0
-    static totalInvitado = 0;
-    static edadPromedio = 0;
-    static usuarioDelMes = "";
+    static totalPlanes = [0, 0, 0];
+    static totalTipos = [0, 0]
 
     // Constructor para crear usuarios
     constructor (nombre, documento, correo, fecha, plan, tipo, descuento) {
@@ -33,6 +27,27 @@ class Usuario {
         this.descuento = descuento;
         this.visitas = 1;
         Usuario.cantidadUsuarios++;
+
+        switch(plan) {
+            case "Básico":
+                Usuario.totalPlanes[0]++;
+                break;
+            case "Pro":
+                Usuario.totalPlanes[1]++;
+                break;
+            case "Premium":
+                Usuario.totalPlanes[2]++;
+                break;
+        }
+
+        switch(tipo) {
+            case "Anfitrión":
+                Usuario.totalTipos[0]++;
+                break;
+            case "Invitado":
+                Usuario.totalTipos[1]++;
+                break;
+        }
     }
 
     // Registrar un usuario en el menú registrar
@@ -61,17 +76,26 @@ class Usuario {
             let usuario = new Usuario(nombre, documento, correo, fecha, plan, tipo, descuento ? "Sí" : "No");
             Usuario.listaUsuarios.push(usuario);
 
-            Usuario.tablaUsuarios.innerHTML += `<tr class="text-center">
+            Usuario.agregarUsuarioTabla(usuario);
+
+            // Limpiar el formulario para futuros usos
+            const inputs = document.querySelectorAll('input');
+            inputs.forEach(input =>  input.value = '');
+        }
+    }
+
+    // Actualizar la tabla
+    static agregarUsuarioTabla(usuario) {
+        Usuario.tablaUsuarios.innerHTML += `<tr class="text-center">
                                                 <td>${usuario.nombre}</td>
                                                 <td>${usuario.documento}</td>
                                                 <td>${usuario.correo}</td>
                                                 <td>${usuario.fecha}</td>
                                                 <td>${usuario.plan}</td>
                                                 <td>${usuario.tipo}</td>
-                                                <td>${usuario.descuento}</td>
+                                                <td>${usuario.descuento ? "Si" : "No"}</td>
                                                 <td>${usuario.visitas}</td>
                                             </tr>`;
-        }
     }
 
     static ingresar() {
@@ -87,6 +111,10 @@ class Usuario {
         // Si no se encuentra se indice. De lo contrario se agrega una nueva visita.
         if (usuarioExistente.length >= 1) {
             usuarioExistente[0].visitas++; // Quedamos debiendo como reemplazar el valor en la tabla
+
+            // Limpiar el input para futuros usos
+            const inputs = document.querySelectorAll('input');
+            inputs.forEach(input =>  input.value = '');
         } else if (documentoIngresar == "") {
             mensajeIngresar.textContent = "Debe ingresar un número de documento!";
         } else {
@@ -94,46 +122,62 @@ class Usuario {
         }
     }
 
-    static recorrerUsuarios(usuario) {
+    static calcularPrecioUsuarios(usuario) {
         const impuesto = Usuario.impuesto;
 
         if (usuario.tipo == "Invitado"){
-            Usuario.totalInvitado++;
             return 0;
         } 
-
-        Usuario.totalAnfitrion = Usuario.cantidadUsuarios - Usuario.totalInvitado;
-
-        Usuario.edad += Usuario.calcularEdad(usuario);
 
         let plan = usuario.plan;
         let descuento = usuario.descuento ? Usuario.descuentoEspecial : 1;
         switch(plan) {
             case "Básico":
-                Usuario.totalBasico++;
                 return Usuario.precioBasico * impuesto * descuento;
             case "Pro":
-                Usuario.totalPro++;
                 return Usuario.precioPro * impuesto * descuento;
             case "Premium":
-                Usuario.totalPremium++;
                 return Usuario.precioPremium * impuesto * descuento;
         }
-
     }
 
-    static calcularEdad(usuario) {
-        let edad = Usuario.año - Number(usuario.fecha.split("-")[0]);
-        return edad;
+    static calcularMasComprado(lista) {
+        let comprasMaximas = lista.reduce((anterior, actual) => {return Math.max(anterior, actual);});
+        let planMaximo = lista.indexOf(comprasMaximas);
+        switch(planMaximo) {
+            case 0:
+                return ["Plan Básico", comprasMaximas];
+            case 1:
+                return ["Plan Pro", comprasMaximas];
+            case 2:
+                return ["Plan Premium", comprasMaximas];
+        }
     }
 
-    static calcularGanancia() {
-        const listaPrecioUsuarios = Usuario.listaUsuarios.map(Usuario.recorrerUsuarios);
-        Usuario.ganancias = listaPrecioUsuarios.reduce((acumulador, valor) => {return acumulador + valor;});
-        Usuario.edadPromedio /= Usuario.cantidadUsuarios;
+    static calcularEdadUsuario(usuario) {
+        let añoNacimiento = Number(usuario.fecha.split("-")[0]);
+        let edadUsuario = Usuario.año - añoNacimiento;
+        return edadUsuario;
     }
+
+    static calcularMasActivo(lista) {
+        let masActivo = lista[0];
+        
+        // Buscamos uno por uno cual es el usuario mas activo
+        for (let u of lista) {
+            if (u.visitas > masActivo.visitas) {
+                masActivo = u;
+            }
+        }
+
+        // Aplicar descuento
+        masActivo.descuento = true;
+
+        return masActivo;
+    } 
 
     static actualizarEstadisticas() {
+        // Etiquetas donde se mostraran los resultados de las estadísticas
         const usuariosActivos = document.getElementById("usuariosActivos");
         const gananciaMes = document.getElementById("gananciaMes");
         const masComprado = document.getElementById("masComprado");
@@ -142,15 +186,57 @@ class Usuario {
         const edadPromedio = document.getElementById("edadPromedio");
         const masActivo = document.getElementById("masActivo");
 
-        Usuario.calcularGanancia();
-        console.log(Usuario.totalBasico, Usuario.totalPro, Usuario.totalPremium);
+        if (Usuario.listaUsuarios.length > 0) {
+            // Cantidad de usuarios
+            let cantidadUsuarios = Usuario.cantidadUsuarios;
 
-        usuariosActivos.textContent = `Tenemos ${Usuario.cantidadUsuarios} usuarios activos este mes`;
-        gananciaMes.textContent = `Se tuvo una ganancia de $${Usuario.ganancias.toFixed(2)} este mes`;
-        masComprado.textContent = `El plan más comprado es: ${Math.max(Usuario.totalBasico, Usuario.totalPro, Usuario.totalPremium)}`;
-        pAnfitrion.textContent = `El porcentaje de anfitriones es de ${(Usuario.totalAnfitrion / Usuario.cantidadUsuarios * 100).toFixed(1)}%`;
-        pInvitado.textContent = `El porcentaje de invitados es de ${(Usuario.totalInvitado / Usuario.cantidadUsuarios * 100).toFixed(1)}%`;
-        edadPromedio.textContent = `La edad promedio es de ${Usuario.edadPromedio} años`;
-        masActivo.textContent = `El usuario del mes es Juancho! Felicidades!`;
+            // Ganancia mensual
+            let ganancia = Usuario.listaUsuarios.map(Usuario.calcularPrecioUsuarios).reduce((acumulador, valor) => {return acumulador + valor;});
+            
+            // Plan mas comprado
+            let planMasComprado = Usuario.calcularMasComprado(Usuario.totalPlanes);
+            let plan = planMasComprado[0];
+            let numeroCompras = planMasComprado[1];
+
+            // Porcentaje anfitriones e invitados
+            let porcAnfitrion = (Usuario.totalTipos[0] / Usuario.cantidadUsuarios * 100).toFixed(1);
+            let porcInvitado = (Usuario.totalTipos[1] / Usuario.cantidadUsuarios * 100).toFixed(1);
+
+            // Edad promedio
+            let edad = Usuario.listaUsuarios.map(Usuario.calcularEdadUsuario).reduce((acumulador, valor) => {return acumulador + valor;});
+            let edadProm = Math.floor(edad / Usuario.cantidadUsuarios);
+            
+            // Usuario más activo
+            let usuarioDelMes = Usuario.calcularMasActivo(Usuario.listaUsuarios);
+            
+            // Aplicando los resultados a las etiquetas para mostrarlos en pantalla
+            usuariosActivos.textContent = `Tuvimos ${cantidadUsuarios} usuarios activos este mes`;
+            gananciaMes.textContent = `Se tuvo una ganancia de $${ganancia} este mes`;
+            masComprado.textContent = `El plan más comprado fue ${plan} con ${numeroCompras} compras`;
+            pAnfitrion.textContent = `El porcentaje de anfitriones fue de ${porcAnfitrion}%`;
+            pInvitado.textContent = `El porcentaje de invitados fue de ${porcInvitado}%`;
+            edadPromedio.textContent = `La edad promedio fue de ${edadProm} años`;
+            masActivo.textContent = `El usuario del mes es ${usuarioDelMes.nombre} con ${usuarioDelMes.visitas} visitas! Felicidades!`;
+        } else {
+            usuariosActivos.textContent = `Aún no hay usuarios registrados`;
+        }
+    }
+
+    static actualizarTabla() {
+        Usuario.tablaUsuarios.innerHTML = `<tr class="bg-[#59C9A5]">
+                                                <th>NOMBRE</th>
+                                                <th>DOCUMENTO</th>
+                                                <th>CORREO</th>
+                                                <th>F. NACIMIENTO</th>
+                                                <th>PLAN</th>
+                                                <th>TIPO</th>
+                                                <th>DESC</th>
+                                                <th>VISITAS</th>
+                                            </tr>`;
+
+        for (let usuario of Usuario.listaUsuarios) {
+            Usuario.agregarUsuarioTabla(usuario);
+        }
+        
     }
 }
